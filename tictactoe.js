b8886c48d159a34f9612fb39bc226841feb0a37d
Chris Johnson <ChrisJohnson0804@gmail.com>
2020-11-17 12:29:27 -0500

    forgot to save, oops

---
+++ tictactoe.js
@@ -0,0 +1,10 @@
+const Gameboard = {
+    gameBoard: [],
+    makeBoard: () => {
+        for(let i=0; i<9; i++){
+            this.gameBoard.push(tile)
+        }
+    }
+};
+
+const tile = document.createElement('div');
